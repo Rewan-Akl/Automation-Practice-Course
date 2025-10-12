755285578129d00f62e3c72447e14d1e1875de31
Rewan Wael Akl <132017721+RewanWael@users.noreply.github.com>
2025-10-12 17:03:12 +0300

    Locators||

--- src/test/java/ElementActions.java
+++ src/test/java/ElementActions.java
@@ -1,8 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.pagefactory.ByAll;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 public class ElementActions {
     WebDriver driver;
@@ -10,10 +13,12 @@ public class ElementActions {
     By Password = By.id("inputPassword");
     By loginButton = By.id("loginButton");
     By loginFields = new ByAll(By.id("inputUsername"), By.id("inputPassword"), By.id("loginButton"));
+    By uploadFilesLink = By.partialLinkText("Upload");
+    By chooseFilesButton = By.id("regularFileInput");
 
 
     @Test
-    public void TestCase() {
+    public void login() {
         driver = new EdgeDriver();
         driver.get("https://ashraaf7.github.io/AA-Practice-Test-Automation/");
         driver.manage().window().maximize();
@@ -27,7 +32,58 @@ public class ElementActions {
         driver.findElements(loginFields).getLast().click();
 
         //   driver.findElement(loginButton).click();
+    }
 
+    @Test
+    public void uploadFileTC() {
+        driver = new EdgeDriver();
+        driver.get("https://ashraaf7.github.io/AA-Practice-Test-Automation/");
+        driver.manage().window().maximize();
+        driver.findElement(userName).sendKeys("admin");
+        driver.findElement(Password).sendKeys("admin");
+        driver.findElement(loginButton).submit();
+        driver.findElement(uploadFilesLink).click();
+        String userHome = System.getProperty("user.dir");
+        driver.findElement(chooseFilesButton).sendKeys(userHome + File.separator + "src\\test\\resources\\test.txt");
+
+    }
+
+    @Test  //After using functions
+    public void uploadFileAfterUsingFunctions() {
+        driver = new EdgeDriver();
+        navigateTo("https://ashraaf7.github.io/AA-Practice-Test-Automation/");
+        driver.manage().window().maximize();
+        type(userName, "admin");
+        type(Password, "admin");
+        clicking(loginButton);
+        clicking(uploadFilesLink);
+        uploadFile(chooseFilesButton, "src\\test\\resources\\test.txt");
+    }
+
+    public WebElement findElement(By by) {
+        return driver.findElement(by);
+    }
+
+    public void navigateTo(String URL) {
+        driver.navigate().to(URL);
+    }
+
+    public void clicking(By by) {
+        driver.findElement(by).click();
+    }
+
+    public void type(By by, String text) {
+        clear(by);
+        driver.findElement(by).sendKeys(text);
+    }
+
+    public void clear(By by) {
+        driver.findElement(by).clear();
+    }
+
+    public void uploadFile(By by, String filePath) {
+        String userHome = System.getProperty("user.dir");
+        driver.findElement(by).sendKeys(userHome + File.separator + filePath);
 
     }
 }
