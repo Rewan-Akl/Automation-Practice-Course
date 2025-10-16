67a7f0313f04fc497c671cd2ddf2589a67c3553c
Rewan Wael Akl <132017721+RewanWael@users.noreply.github.com>
2025-10-16 11:30:02 +0300

    JavaScript Executer

--- src/test/java/JavaScriptExecuter.java
+++ src/test/java/JavaScriptExecuter.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
 
 public class JavaScriptExecuter {
+    WebDriver driver;
+
+    @Test
+    public void JS() {
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://ashraaf7.github.io/AA-Practice-Test-Automation/Pages/scrolling.html");
+        ((JavascriptExecutor) driver).
+                executeScript("arguments[0].scrollIntoView();", driver.findElement(By.id("scroll_text")));
+    }
 }
